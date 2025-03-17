0672f9ea76829300015d84c57d494eb803f63a95
guilleunfv <60369392+guilleunfv@users.noreply.github.com>
2025-03-17 14:07:23 -0300

    codigo mejorado main.py

--- cloudrun/main.py
+++ cloudrun/main.py
@@ -5,6 +5,7 @@ import logging
 from datetime import datetime
 
 from flask import Flask
+
 app = Flask(__name__)
 
 # Configura logging
@@ -38,26 +39,33 @@ def atualizar_dados_ibovespa():
             logging.error("Não foi possível baixar dados do IBOVESPA.")
             return "Erro: Não foi possível baixar dados do IBOVESPA.", 500
 
+        # Resetar índice para transformar o índice de datas em uma coluna
         data.reset_index(inplace=True)
 
+        # Filtrar e renomear colunas relevantes
         df = data[["Date", "Open", "High", "Low", "Close", "Volume"]]
-        df['Date'] = pd.to_datetime(df['Date']).dt.strftime('%Y-%m-%d')
+        df['Date'] = pd.to_datetime(df['Date']).dt.strftime('%Y-%m-%d')  # Formatar datas como string
 
         logging.info("Dados baixados e formatados corretamente.")
 
-        # Inserir os dados no BigQuery
+        # Converter valores para tipos compatíveis com BigQuery
+        df = df.astype({
+            "Open": float,
+            "High": float,
+            "Low": float,
+            "Close": float,
+            "Volume": int
+        })
+
+        # Converter DataFrame para lista de dicionários
         data_records = df.to_dict(orient='records')
 
-        for row in data_records:
-            row['Open'] = float(row['Open'])
-            row['High'] = float(row['High'])
-            row['Low'] = float(row['Low'])
-            row['Close'] = float(row['Close'])
-            row['Volume'] = int(row['Volume'])
-
+        # Obter referência da tabela no BigQuery
         table = client.get_table(TABLE_URI)
+
+        # Inserir dados no BigQuery
         errors = client.insert_rows(table, data_records)
-        if errors == []:
+        if not errors:
             logging.info("Dados inseridos com sucesso no BigQuery.")
             return "Dados inseridos com sucesso no BigQuery."
         else:
@@ -65,5 +73,5 @@ def atualizar_dados_ibovespa():
             return f"Ocorreram erros ao inserir dados no BigQuery: {errors}", 500
 
     except Exception as e:
-        logging.error(f"Erro durante la ejecución: {e}")
-        return f"Erro al actualizar los datos: {e}", 500
+        logging.error(f"Erro durante a execução: {e}")
+        return f"Erro ao atualizar os dados: {e}", 500
